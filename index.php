a79b901693fb89c82abe7b92b0f556a45d39e8ce
Camila Fonte <kamilafonte@gmail.com>
2020-09-04 18:37:27 -0300

    Implementada a função de cálculo de fatorial

--- index.php
+++ index.php
@@ -8,20 +8,21 @@ $climate = new League\CLImate\CLImate;
 
 // menu
 $climate->out('[1 - RAIZ QUADRADA]');
-$climate->out('[2 - FATORAÇÃO]');
+$climate->out('[2 - FATORIAL]');
 $climate->out('[3 - AO QUADRADO]');
 
 // escolher opção
 $input = $climate->input('Escolha uma das opções acima:');
 
 // opções aceitas
-$input->accept(['1','2','3']);
+$input->accept(['1', '2', '3']);
 
 
 // receber o que foi inserido
 $menu = $input->prompt();
 
-class Calculo {
+class Calculo
+{
     public $numero;
 
     protected $climate;
@@ -31,7 +32,8 @@ class Calculo {
         $this->climate = $climate;
     }
 
-    public function input() {
+    public function input()
+    {
         $numeroInput  = $this->climate->input('Insira o número a ser calculado:');
         $this->numero = $numeroInput->prompt();
     }
@@ -41,10 +43,28 @@ $calculo = new Calculo($climate);
 $calculo->input();
 
 switch ($menu) {
+    // raiz quadrada
     case '1':
-        echo ("O resultado é ".sqrt ($calculo->numero ));
+        echo ("O resultado é " . sqrt($calculo->numero));
         break;
-    
+
+    case '2':
+        // fatorial
+        $numeroInput = $climate->input('Insira o número que deseja receber o fatorial: ');
+        
+        $numero = $numeroInput->prompt();
+
+        $numero = $numero;
+
+        $fatorial = 1;
+
+        for ($i = 1; $i <= $numero; $i++) {
+            $fatorial = $fatorial * $i;
+        }
+
+        echo ("O fatorial é: $fatorial");
+        break;
+
     default:
         # code...
         break;
